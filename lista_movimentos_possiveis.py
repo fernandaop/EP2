707878f2ab685db74b807f16e8b1a361113febcc
Fernanda <fernandaop@al.insper.edu.br>
2021-04-28 11:03:41 -0300

    Reposição do quarto commit

--- lista_movimentos_possiveis.py
+++ lista_movimentos_possiveis.py
@@ -1,9 +1,16 @@
+from extrai_valor import extrai_valor as ev
+from extrai_naipe import extrai_naipe as en
 def lista_movimentos_possiveis(lista, x):
     lista1 = []
     if x == 0:
         return lista1
-    elif lista[x][0] == lista[x-1][0] or lista[x][-1] == lista[x-1][-1]:
-        lista1.append(1)
-        if lista[x][0] == lista[x-3][0] and x > 2 or lista[x][-1] == lista[x-3][-1] and x > 2:
-            lista1.append(3)
-    return lista1
+    elif 0 < x < 3:
+        if ev(lista[x]) == ev(lista[x-1]) or en(lista[x]) == en(lista[x-1]):
+            lista1.append(1)
+        return lista1
+    else:
+        if ev(lista[x]) == ev(lista[x-1]) or en(lista[x]) == en(lista[x-1]):
+            lista1.append(1)
+            if ev(lista[x]) == ev(lista[x-3]) or en(lista[x]) ==en(lista[x-3]):
+                lista1.append(3)
+        return lista1
